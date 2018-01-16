c39a9d8440e56a685320afb0c17eaa0fa9cd8dfc
Eli Cleveland <elicleveland12@gmail.com>
2018-01-16 21:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const app = "I don't do much."
+const app = "I don't do much.";
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return kittens
+  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.unshift(name)
-  return kittens
+  kittens.unshift(name);
+  return kittens;
 }
 
 function destructivelyRemoveLastKitten() {
   kittens.pop();
-  return kittens
+  return kittens;
 }
 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
-  return kittens
+  return kittens; 
 }
 
 function appendKitten(name) {
